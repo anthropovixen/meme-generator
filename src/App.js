991003c6bf4cc897317f39f96a0f97db7541c69d
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-02-15 16:11:52 +1100

    Update App.js
    Add meme component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import Meme from "./components/Meme";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Meme />
     </div>
-  )
+  );
 }
