ea984ab42f29ccc752954f7b5007984a39e31a34
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-08 15:14:44 +1100

    Update App.js
    Change header name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import Meme from "./components/Meme";
 
 export default function App() {
   return (
     <div>
-      <Navbar />
+      <Header />
       <Meme />
     </div>
   );
