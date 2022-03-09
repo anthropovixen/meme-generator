aba526688b37b6a354cb865ad28c8ba07615e19a
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-03-09 15:17:28 +1100

    Update Meme.js
    import use effect
    remove memesData to use API to fetch data to state
    use effect to fetch data from API, transforming it into json object, then passing the array of data to state
    Remove the array transformation from getMemeImage to go straight from the state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import memesData from "../memesData";
+import React, { useState, useEffect } from "react";
 
 export default function Meme() {
   const [meme, setMeme] = useState({
@@ -8,12 +7,18 @@ export default function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemeImages, setAllMemeImages] = useState(memesData);
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    console.log("Effect ran");
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
 
   function getMemeImage() {
-    const memesArray = allMemeImages.data.memes;
-    const RandomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[RandomNumber].url;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
     setMeme((prevMeme) => ({
       ...prevMeme,
       randomImage: url,
